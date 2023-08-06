3b70e87e56a17a64cfc8af788d7b31126893b82e
Rayelisson <33360322+Rayelisson@users.noreply.github.com>
2023-08-05 23:24:06 -0300

    parte do dia 05/08/23

---
+++ src/models/login/screens/LoginScreen.tsx
@@ -0,0 +1,25 @@
+import Input from '../../../shared/inputs/input/input';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  ContainerLoginScreen,
+  LimitedContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+  return (
+    <ContainerLoginScreen>
+      <BackgroundImage src="./background.png" />
+      <ContainerLogin>
+        <LimitedContainer>
+          <LogoImage src="./logo.png" />
+          <Input title="USUARÍO" />
+          <Input title="SENHA" />
+        </LimitedContainer>
+      </ContainerLogin>
+    </ContainerLoginScreen>
+  );
+};
+
+export default LoginScreen;
